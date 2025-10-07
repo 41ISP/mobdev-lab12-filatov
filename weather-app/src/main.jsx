b5db7cb0f563ca0d352f59cd979e4064897b57f1
Roman <itz.ruzik@vk.com>
2025-10-07 08:09:39 +0000

    tg-app

--- weather-app/src/main.jsx
+++ weather-app/src/main.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./App.css";
+import { TelegramProvider } from "./context/TelegramContext.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<TelegramProvider/>);
