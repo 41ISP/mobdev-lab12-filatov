093433d18421d6b02fcaeb148f985a27cf95d432
Roman <itz.ruzik@vk.com>
2025-10-07 08:41:26 +0000

    tg-app

--- weather-app/vite.config.js
+++ weather-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'mobdev-lab11-filatov'
+  base: 'mobdev-lab12-filatov'
 })
